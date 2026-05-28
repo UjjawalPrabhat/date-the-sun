e5ea145b6491b2a7f08575f870f8c82195b04ca1
RyanDJ <heryandjaruma@gmail.com>
2026-05-28 14:24:34 +0800

    add clock capability

---
+++ date-the-sun/Component/IndoorOutdoorClock.swift
@@ -0,0 +1,18 @@
+//
+//  IndoorOutdoorClock.swift
+//  date-the-sun
+//
+//  Created by Heryan Djaruma on 28/05/26.
+//
+
+import SwiftUI
+
+struct IndoorOutdoorClock: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    IndoorOutdoorClock()
+}
